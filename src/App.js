cf6c89ee6da7f382839d392a14f51c138ae56f37
AliceMed03 <alicemedeirosds@gmail.com>
2023-06-21 21:21:01 -0300

    Atualização InclusaoLivros.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MenuSuperior from './components/MenuSuperior';
+import InclusaoLivros from './components/InclusaoLivros';
 
 //function App() {
 const App = () => {
   return ( //tudo que vai no return é o que aparece na aplicação
     <>
     <MenuSuperior/>
+    <InclusaoLivros/>
     </>
   );
 }
